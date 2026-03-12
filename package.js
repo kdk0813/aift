1f078aa5a29393521f03bac9ba5c0413dfb390d5
kdk0813 <spacer7379@gmail.com>
2026-03-12 15:39:49 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+"name": "render-neon-test",
+"version": "1.0.0",
+"description": "Simple web service using Render and Neon",
+"main": "index.js",
+"scripts": {
+"start": "node index.js"
+},
+"author": "",
+"license": "ISC",
+"dependencies": {
+"express": "^4.19.2",
+"pg": "^8.11.5"
+}
+}
